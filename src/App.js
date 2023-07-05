18ae11ddc8de5af884f5c11b60b458f58152ba2a
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-07-05 20:56:15 +0100

    link added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Weather from "./Weather";
+import Link from "./Link";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Weather />
+    <Link />
     
         
     </div>
---
+++ src/Link.js
@@ -0,0 +1,13 @@
+
+
+export default function showLink (){
+    return (
+      <div>
+        <footer>
+          Open-source code by 
+          <a href="https://github.com/EduardMat/react-weather"> Sefora Noata </a>
+         on GitHub
+        </footer>
+      </div>
+    );
+}
